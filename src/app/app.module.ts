a154ea4fcc28c7a748a9b30f9597a962463b8234
anaghacm <anagharsnair@gmail.com>
2023-02-08 16:28:48 +0530

    Beinex-Task-Feb8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildOneComponent } from './child-modules/child-one/child-one.component';
+import { ChildTwoComponent } from './child-modules/child-two/child-two.component';
+import { ChildThreeComponent } from './child-modules/child-three/child-three.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent,
+    ChildOneComponent,
+    ChildTwoComponent,
+    ChildThreeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
